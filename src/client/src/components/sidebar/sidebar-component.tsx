08653d32c9a06ae0543792d60ed7c4c9c4765c7b
Rikonie <rikonie@bk.ru>
2021-08-18 18:09:01 +0300

    Дополнение к предыдущему коммиту, еще страницы

---
+++ src/client/src/components/sidebar/sidebar-component.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const SideBarComponent = () => {
+    return (
+        <ul>
+                    <li><Link to="/home">Главная</Link></li>
+                    <li><Link to="/guild">Гильдия</Link></li>
+                    <li><Link to="/timetable">Расписание рейдов</Link></li>
+                    <li><Link to="/composition">Состав</Link></li>
+                    <li><Link to="/statistics">Статистика</Link></li>
+                    <li><Link to="/calculator">Кальлкулятор рейда</Link></li>
+                    <li><Link to="/nextdoor">piska Бродячего</Link></li>
+        </ul>
+    );
+}
