0750d2e7f37beb4f53fc8b92e03351bbe1497e54
Anatoly Kozachenko <anatoly.kozachenko@arcadia.spb.ru>
2021-08-19 12:24:37 +0300

    React component for selection

--- src/client/src/components/class-select/class-select-component.tsx
+++ src/client/src/components/class-select/class-select-component.tsx
@@ -1,20 +1,52 @@
 import styles from ".//class-select.module.scss";
 import React from "react";
-export const ClassSelectComponent = () => {
+
+export enum Clazzes {
+    Warrior = 'warrior',
+    Hunter = 'hunter',
+    Mage = 'mage',
+    Rogue = 'rogue',
+    Priest = 'priest',
+    Warlock = 'warlock',
+    Paladin = 'paladin',
+    Druid = 'druid',
+    Shaman = 'shaman',
+    Monk = 'monk',
+    DeathKnight = 'dk',
+    DemonHunter = 'dh'
+}
+export class ClassSelectComponentProps {
+    onSelect?: (selected: Clazzes) => void;
+}
+
+export const ClassSelectComponent: React.FC<ClassSelectComponentProps> = ({onSelect}) => {
+
+    const classNames = Object.keys(Clazzes).map(key => Clazzes[key]);
+
+    const onClassSelect = (event)=>{
+        console.log(event.target.value);
+    }
+
+    // return <select>
+    //     <option className={styles[Clazzes.Warrior]} value={Clazzes.Warrior}>Воин</option>
+    //     <option className={styles[Clazzes.Hunter]} value={Clazzes.Hunter}>Охотник</option>
+    //     <option className={styles[Clazzes.Mage]} value={Clazzes.Mage}>Маг</option>
+    //     <option className={styles.rogue}>Разбойник</option>
+    //     <option className={styles.priest}>Жрец</option>
+    //     <option className={styles.warlock}>Чернокнижник</option>
+    //     <option className={styles.paladin}>Паладин</option>
+    //     <option className={styles.druid}>Друид</option>
+    //     <option className={styles.shaman}>Шаман</option>
+    //     <option className={styles.monk}>Монах</option>
+    //     <option className={styles.dh}>Охотник на демонов</option>
+    //     <option className={styles.dk}>Рыцарь смерти</option>
+    // </select>
+
     return (
-        <select>
-        <option className={styles.warrior} value="0">Воин</option>
-        <option className={styles.hunter}  value="1">Охотник</option>
-        <option className={styles.mage} value="2">Маг</option>
-        <option className={styles.rogue}>Разбойник</option>
-        <option className={styles.priest}>Жрец</option>
-        <option className={styles.warlock}>Чернокнижник</option>
-        <option className={styles.paladin}>Паладин</option>
-        <option className={styles.druid}>Друид</option>
-        <option className={styles.shaman}>Шаман</option>
-        <option className={styles.monk}>Монах</option>
-        <option className={styles.dh}>Охотник на демонов</option>
-        <option className={styles.dk}>Рыцарь смерти</option>
+        <select onChange={onClassSelect}>
+            {classNames.map(k=>{
+                return <option className={styles[k]} value={k}>{k}</option>
+            })}
         </select>
     );
 };
--- src/client/src/components/dickpic/dickpic-component.tsx
+++ src/client/src/components/dickpic/dickpic-component.tsx
@@ -1,46 +1,12 @@
 import styles from ".//dickpic.module.scss";
 import React from "react";
-import { ClassSelectComponent } from "../../components/class-select/class-select-component";
+import {ClassSelectComponent, Clazzes} from "../../components/class-select/class-select-component";
 
 export const DickPicComponent = () => {
 
-    if (ClassSelectComponent[0].selected){
+    const onClazzSelect = (selected: Clazzes): void => {
+        console.log('props');
+    };
 
-        return (
-
-            <div>
-                <img src="https://cdn0.iconfinder.com/data/icons/coloricons/50/50X50-10-256.png"/>
-            </div>
-        )
-    }
-
-else if (ClassSelectComponent[1].selected){
-
-        return (
-
-            <div>
-                <img src="https://cdn3.iconfinder.com/data/icons/systematrix/WoW.png"/>
-            </div>
-        )
-    }
-
-else if (ClassSelectComponent[2].selected){
-
-        return (
-
-            <div>
-                <img src="https://cdn0.iconfinder.com/data/icons/coloricons/50/50X50-12-256.png"/>
-            </div>
-        )
-    }
-
-    else {
-
-        return (
-
-            <div>
-                <img src="https://cdn4.iconfinder.com/data/icons/smashicons-game-flat/60/41_-_For_the_Horde_Flat-256.png"/>
-            </div>
-        )
-    }
+    return <ClassSelectComponent onSelect={onClazzSelect}/>
 };
