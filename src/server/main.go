bb653aacff9a581f82580464d6dcac09a801398a
Anatoly Kozachenko <anatoly.kozachenko@arcadia.spb.ru>
2021-08-27 21:49:31 +0300

    simple server

---
+++ src/server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
+	"html"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	http.HandleFunc("/raiders", returnRaiders)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
+
+func returnRaiders(w http.ResponseWriter, r *http.Request) {
+	raiders := getRaiders()
+	json.NewEncoder(w).Encode(raiders)
+}
+
+func getRaiders() []Raider {
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := sqlx.Connect("pgx", "postgres://postgres:postgres_123@localhost:5432/raid")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	var raiders []Raider
+	err = conn.Select(&raiders, "select * from guildmates")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	return raiders
+}
+
+type Raider struct {
+	Name    string `db:"name"`
+	Id      int64  `db:"id"`
+	ClassId int64  `db:"classId"`
+	Level   int64  `db:"level"`
+	Rank    int64  `db:"rank"`
+	RaceId  int64  `db:"raceId"`
+}
