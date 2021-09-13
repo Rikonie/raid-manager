f2d797ce71ab690812b4a82721c981709b880afa
Rikonie <rikonie@bk.ru>
2021-09-13 19:53:51 +0300

    Календарь на странице пока без событий

--- src/client/src/pages/timetable/timetable-page.tsx
+++ src/client/src/pages/timetable/timetable-page.tsx
@@ -1,8 +1,39 @@
 import React from "react";
+import {Calendar, momentLocalizer,} from "react-big-calendar";
+
+import 'react-big-calendar/lib/sass/styles.scss';
+import moment from 'moment'
+
+class Event {
+    title: string;
+    start: Date;
+    end: Date;
+    allDay?: boolean;
+    resource?: any;
+
+    constructor(title:string, start: Date, end: Date, allDay = false, resource: any) {
+        this.title = title;
+        this.start = start;
+        this.end = end;
+        this.allDay = allDay;
+        this.resource =  resource;
+    }
+}
+
+const localizer = momentLocalizer(moment);
+const event : Event= new Event("sdfs", new Date(1631810400000), new Date(1631810400000+10800000), false, null );
 
 export const TimetablePage = () => {
     return (
         <div>
+            <Calendar
+                localizer={localizer}
+                events={[event]}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
+
         </div>
     )
 };
